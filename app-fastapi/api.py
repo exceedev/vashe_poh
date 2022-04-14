305542d104c348e58029506799ad19d178010d57
AntonGrebenyukov <agrebenyukov.exceedteam@gmail.com>
2022-04-14 15:28:54 +0300

    fixed codeql

--- app-fastapi/api.py
+++ app-fastapi/api.py
@@ -1,4 +1,5 @@
 import io
+import os
 import shutil
 import gridfs
 from fastapi import APIRouter, UploadFile, File
@@ -6,14 +7,19 @@ from starlette.responses import StreamingResponse
 
 from db import image_db
 from tasks import test
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 
-image_router = APIRouter()
+image_router = APIRouter(
+    prefix='/images'
+)
 
 database = gridfs.GridFS(image_db)
+DOWNLOAD_LOCATION = os.getenv('DOWNLOAD_LOCATION')
 
 
-@image_router.get('/img')
+@image_router.get('/')
 async def get_images():
     response = database.find_one({})
     binary_data = response.read()
@@ -21,18 +27,10 @@ async def get_images():
     return StreamingResponse(io.BytesIO(binary_data))
 
 
-@image_router.post('/upload')
+@image_router.post('/upload/')
 async def upload_file(file: UploadFile = File(...)):
     download_location = f'media/{file.filename}'
     with open(download_location, 'wb') as output:
         data = database.find_one({'filename': file.filename})
         shutil.copyfileobj(data, output)
     database.put(file.file, filename=file.filename)
-
-
-@image_router.get('/download')
-async def download_file(name: str):
-    download_location = f'media/{name}'
-    with open(download_location, 'wb') as output:
-        data = database.find_one({'filename': name})
-        shutil.copyfileobj(data, output)
