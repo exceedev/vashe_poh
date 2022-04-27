12ef6a0316034e509de45ffbc8384bc322413dbd
AntonGrebenyukov <agrebenyukov.exceedteam@gmail.com>
2022-04-27 11:32:29 +0300

    get and response filename

--- app-fastapi/api.py
+++ app-fastapi/api.py
@@ -1,18 +1,26 @@
+from bson import ObjectId
 from fastapi import APIRouter, UploadFile, File
 
 from db import collection
 from services import save_image
-from schemas import UploadImage
+from schemas import GetImage, PydanticObjectId
+
 
 image_router = APIRouter(
     prefix='/images'
 )
 
 
-@image_router.post('/upload/', response_model=UploadImage, status_code=201)
+@image_router.post('/upload/', response_model=GetImage, status_code=201)
 async def upload_file(file: UploadFile = File(...)):
-    await save_image(file)
-    document = {'filename': file.filename}
+    file_save = await save_image(file)
+    document = {'filename': file_save}
     object_ = await collection.insert_one(document)
-    document |= {"object_id": object_.inserted_id}
+    document |= {'id': object_.inserted_id}
     return document
+
+
+@image_router.get('/{image_id}/', response_model=GetImage)
+async def get_image(image_id: str):
+    result = await collection.find_one({'_id': ObjectId(image_id)})
+    return result
