c27c331b2b71c5ca755390434aed36f4951842e5
amiktadov <amiktadov.exceedteam@gmail.com>
2022-04-28 13:08:03 +0300

    add Refresh JWT system

--- flask_os/views.py
+++ flask_os/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_refresh_token
 
 from .models import *
 
@@ -7,10 +7,10 @@ from .models import *
 users_blueprint = Blueprint('users', __name__)
 
 
-@users_blueprint.route('/test', methods=['GET', 'POST'])
+@users_blueprint.route('/test/<int:count>', methods=['GET', 'POST'])
 @jwt_required()
-def test():
-    return jsonify({'count': 'count'})
+def test(count):
+    return jsonify({'count': count})
 
 
 @users_blueprint.route('/register', methods=['POST'])
@@ -27,5 +27,15 @@ def register():
 def login():
     params = request.json
     user = User.authenticate(**params)
+    refresh = create_refresh_token(identity=user.id)
     token = user.get_token()
-    return {'access_token': token}
+    return {'access_token': token, 'refresh_token': refresh}
+
+
+@users_blueprint.route('/token/refresh')
+@jwt_required(refresh=True)
+def refresh_users_token():
+    identity = get_jwt_identity()
+    access = create_access_token(identity=identity)
+
+    return jsonify({'refresh_token': access})
