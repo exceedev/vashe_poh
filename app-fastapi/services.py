9fafdb421bc04a165007f9e011561ef160c3393b
AntonGrebenyukov <agrebenyukov.exceedteam@gmail.com>
2022-05-13 17:16:24 +0300

    background task

--- app-fastapi/services.py
+++ app-fastapi/services.py
@@ -3,6 +3,7 @@ import os
 
 from uuid import uuid4
 from fastapi import UploadFile, HTTPException
+from tasks import crop_image
 
 
 CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/heic']
@@ -12,9 +13,13 @@ async def save_image(file: UploadFile):
     if not os.path.exists('media/'):
         os.makedirs('media')
     filename = f'{uuid4()}.jpeg'
-    filepath = f'media/{filename}'
+    # filepath = f'media/{filename}'
+    file_bytes = file.file.read()
     if file.content_type in CONTENT_TYPES:
-        await write_image(filepath, file)
+        crop_image.apply_async(
+            queue='high_priority', args=(str(file_bytes), filename)
+        )
+
     else:
         raise HTTPException(status_code=412, detail='Incorrect image type')
     return filename
--- app-fastapi/tasks.py
+++ app-fastapi/tasks.py
@@ -1,6 +1,17 @@
+from uuid import uuid4
+
+from PIL import Image
+from fastapi import UploadFile
+
 from celery_settings import app
 
 
 @app.task
-def test():
-    return 'Backgroung task succeeded'
+def crop_image(file_bytes: bytes, filename: str):
+    # filename = f'{uuid4()}.jpeg'
+    filepath = f'media/{filename}'
+    # file_bytes = file.file.read()
+    image = Image.open(file_bytes)
+    croped_image = image.resize((600, 400))
+    croped_image.save(filepath)
+    return filename
